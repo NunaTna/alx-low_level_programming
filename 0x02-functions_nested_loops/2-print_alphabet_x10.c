4bd2bf9d496c3edff5aee342b5496a15ec5fe065
NunaTna <tseganesh.abera@outlook.com>
2022-10-19 14:43:22 -0700

    Tenx alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alpha x10
+ * @void: no arguments.
+ *
+ * Return: no return value.
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(alphabet[j]);
+		j++;
+	}
+	_putchar('\n');
+	i++;
+}
+}
