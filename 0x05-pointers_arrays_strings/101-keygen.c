37bd8671536f37edc79ffe2bb751648a351533ee
NunaTna <tseganesh.abera@outlook.com>
2022-10-24 16:08:24 -0700

    Last one

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+ * main - print password
+ * Return: 0
+ */
+int main(void)
+{
+	int ascii = 1050, i = 0, j, random;
+	char password[200];
+	time_t t;
+
+	srand((int) time(&t));
+	while (ascii > 150)
+	{
+		random = rand() % 150;
+		password[i] = random;
+		ascii -= random;
+		i++;
+	}
+	if (ascii > 0)
+		password[i] = ascii;
+	else
+	{
+		i--;
+	}
+
+	for (j = 0; j <= i; j++)
+	{
+		printf("%c", password[j]);
+	}
+	return (0);
+}
