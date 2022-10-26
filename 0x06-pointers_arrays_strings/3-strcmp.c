eff76f56659be53d01f08d19fc4446f7e612602b
NunaTna <tseganesh.abera@outlook.com>
2022-10-26 10:00:33 -0700

    Third task sec trail

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings values
+ * _strcmp - compares two string values
  * @s1: string 1
  * @s2: string 2
  * Return: the difference in s1 and s2
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 	a = 0;
 	while (s1[a] != '\0' && s2[a] != '\0')
 	{
-		if (si[a] != s2[a])
+		if (s1[a] != s2[a])
 		{
 			return (s1[a] - s2[a]);
 		}
